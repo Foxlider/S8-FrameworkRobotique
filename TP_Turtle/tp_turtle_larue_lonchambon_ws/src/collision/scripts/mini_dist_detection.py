1f6ccbb5d1a7e4dca1f556af790e88e5a6f4e016
Keelah <al.loncham@gmail.com>
2023-04-19 11:19:16 +0200

    Classed encore

--- TP_Turtle/tp_turtle_larue_lonchambon_ws/src/collision/scripts/mini_dist_detection.py
+++ TP_Turtle/tp_turtle_larue_lonchambon_ws/src/collision/scripts/mini_dist_detection.py
@@ -4,42 +4,36 @@ import rospy
 import numpy as np
 import math
 from sensor_msgs.msg import LaserScan
+from kobuki_msgs.msg import Sound
 from std_msgs.msg import Float32
 
+class MinDistDetector:
 
-def callback(laserScanMsg):
+    def __init__(self):
+        self.pub = rospy.Publisher('mobile_base/commands/sound', Sound, queue_size=1)
+        rospy.init_node('min_dist_detection', anonymous=True)
+        rospy.Subscriber("scan", LaserScan, self.callback, queue_size=1)
+        rospy.loginfo(rospy.get_caller_id() + " started")
+        rospy.spin()
 
-    # Get minimal value from all the laser ranges
-    ranges = laserScanMsg.ranges
-    minvalue = np.nanmin(ranges)
+    def callback(self, laserScanMsg):
+        ranges = laserScanMsg.ranges
+        minvalue = np.nanmin(ranges)
 
-    # log value to term
-    rospy.loginfo(rospy.get_caller_id() + "\nminvalue %s", minvalue)
+        rospy.loginfo(rospy.get_caller_id() + "\nminvalue %s %s", minvalue, type(minvalue))
 
-    # publish to /min_dist
-    pub.publish(minvalue)
-
-    
-def listener():
-    rospy.init_node('min_dist_detection', anonymous=True)
-
-    #Subscribe to laser scan topic and only keep the last message
-    rospy.Subscriber("scan", LaserScan, callback, queue_size=1)
-
-    # Keepalive
-    rospy.spin()
+        cmd = Sound()
+        cmd.value = 3
+        self.pub.publish(cmd)
+        if(not np.isnan(minvalue)):
+            rospy.sleep(minvalue/5)
 
 def main(args=None):
-    s
-
-
-if __name__ == '__main__':
     try:
-        # Publisher
-        pub = rospy.Publisher('min_dist', Float32, queue_size=1)
-
-        #That does NOT log
-        rospy.loginfo(rospy.get_caller_id() + " started")
-        listener()
+        detector = MinDistDetector()
     except rospy.ROSInterruptException:
         pass
+
+if __name__ == '__main__':
+    main()
+
