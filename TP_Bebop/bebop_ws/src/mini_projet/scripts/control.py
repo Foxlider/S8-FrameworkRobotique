edef458b93939ea71653ef9b033352f4d7388dd8
Keelah <al.loncham@gmail.com>
2023-04-19 11:26:42 +0200

    classed bebop

--- TP_Bebop/bebop_ws/src/mini_projet/scripts/control.py
+++ TP_Bebop/bebop_ws/src/mini_projet/scripts/control.py
@@ -44,81 +44,72 @@ pubLand = None
 pubTakeoff = None
 pubTricks = None
 pub = None
-# def talker():
-#     pub = rospy.Publisher('chatter', String, queue_size=10)
-#     rospy.init_node('talker', anonymous=True)
-#     rate = rospy.Rate(10) # 10hz
-#     while not rospy.is_shutdown():
-#         hello_str = "hello world %s" % rospy.get_time()
-#         rospy.loginfo(hello_str)
-#         pub.publish(hello_str)
-#         rate.sleep()
 
-def callback(joy):
-    """
-        The joy/game-pad message callback.
 
-        :type   joy:    Joy
-        :param  joy:    The incoming joy message.
+
+
+class DroneControllerSim():
+    def __init__(self):
+        rospy.init_node('control_sim', anonymous=True)
+
+        # Publishers
+        self.pubTakeoff = rospy.Publisher('bebop/takeoff', Empty, queue_size=1)
+        self.pubLand = rospy.Publisher('bebop/land', Empty, queue_size=1)
+        # pubTricks = rospy.Publisher('bebop/', Empty,queue_size=1)
+        self.pub = rospy.Publisher('bebop/cmd_vel', Twist, queue_size=2)
+
+        # Subscribers
+        self.sub = rospy.Subscriber("/bebop2/joy", Joy, self.callback, queue_size=2)
+
+    def callback(self, joy):
+        """
+            The joy/game-pad message callback.
+
+            :type   joy:    Joy
+            :param  joy:    The incoming joy message.
 
         """
-    axes = joy.axes
-    buttons = joy.buttons
-    rospy.logdebug(rospy.get_caller_id() + "I heard %s", joy.axes)
-    rospy.logdebug(rospy.get_caller_id() + "I heard %s", joy.buttons)
+        axes = joy.axes
+        buttons = joy.buttons
+        rospy.logdebug(rospy.get_caller_id() + "I heard %s", joy.axes)
+        rospy.logdebug(rospy.get_caller_id() + "I heard %s", joy.buttons)
 
-    if(buttons[SELECT] and buttons[START]):
-        land = Empty()
-        rospy.loginfo(rospy.get_caller_id() + "  STOP !!")
-        pubLand.publish(land)
-        return
-    if(buttons[SELECT]):
-        land = Empty()
-        rospy.loginfo(rospy.get_caller_id() + "  LANDING")
-        pubLand.publish(land)
-        return
-    if(buttons[START]):
-        takeoff = Empty()
-        rospy.loginfo(rospy.get_caller_id() + "  TAKING OFF")
-        pubTakeoff.publish(takeoff)
-        return
-    
-    if(buttons[L_BUMPER] and buttons[BTN_X]):
-        rospy.loginfo(rospy.get_caller_id() + "  FLIP LEFT")
-        return
-    
-    cmd = Twist()
+        if(buttons[SELECT] and buttons[START]):
+            land = Empty()
+            rospy.loginfo(rospy.get_caller_id() + "  STOP !!")
+            self.pubLand.publish(land)
+            return
+        if(buttons[SELECT]):
+            land = Empty()
+            rospy.loginfo(rospy.get_caller_id() + "  LANDING")
+            self.pubLand.publish(land)
+            return
+        if(buttons[START]):
+            takeoff = Empty()
+            rospy.loginfo(rospy.get_caller_id() + "  TAKING OFF")
+            self.pubTakeoff.publish(takeoff)
+            return
+        
+        if(buttons[L_BUMPER] and buttons[BTN_X]):
+            rospy.loginfo(rospy.get_caller_id() + "  FLIP LEFT")
+            return
+        
+        cmd = Twist()
 
-    cmd.linear.x = axes[L_STICK_Y]
-    cmd.linear.y = axes[L_STICK_X]
-    cmd.linear.z = -axes[TRIGGERS]
-    cmd.angular.z = axes[R_STICK_X]
-    rospy.loginfo(rospy.get_caller_id() + " %f|%f|%f -- %f", cmd.linear.x, cmd.linear.y, cmd.linear.z, cmd.angular.z )
-    pub.publish(cmd)
+        cmd.linear.x = axes[L_STICK_Y]
+        cmd.linear.y = axes[L_STICK_X]
+        cmd.linear.z = -axes[TRIGGERS]
+        cmd.angular.z = axes[R_STICK_X]
+        rospy.loginfo(rospy.get_caller_id() + " %f|%f|%f -- %f", cmd.linear.x, cmd.linear.y, cmd.linear.z, cmd.angular.z )
+        self.pub.publish(cmd)
 
-    
-    
-def listener():
 
-    # In ROS, nodes are uniquely named. If two nodes with the same
-    # name are launched, the previous one is kicked off. The
-    # anonymous=True flag means that rospy will choose a unique
-    # name for our 'listener' node so that multiple listeners can
-    # run simultaneously.
-    rospy.init_node('control_sim', anonymous=True)
 
-    rospy.Subscriber("/bebop2/joy", Joy, callback)
+def main(args=None):
 
-    # spin() simply keeps python from exiting until this node is stopped
+    controller= DroneControllerSim()
+
     rospy.spin()
 
 if __name__ == '__main__':
-    try:
-        pubTakeoff = rospy.Publisher('bebop/takeoff', Empty, queue_size=1)
-        pubLand = rospy.Publisher('bebop/land', Empty, queue_size=1)
-        pubTricks = rospy.Publisher('bebop/', Empty,queue_size=1)
-        pub = rospy.Publisher('bebop/cmd_vel', Twist, queue_size=10)
-        
-        listener()
-    except rospy.ROSInterruptException:
-        pass
+    main()
